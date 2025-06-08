2d7e9b8e214defc63d5c578cb80a5d07db2d32f1
SanjarKeySkills <forest2004@mail.ru>
2025-06-08 22:00:18 +0600

    08/06/25| lorem10 test page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,3 +1,11 @@
 export default function ProfilePage() {
-    return <div>Profile Page</div>;
+    return (
+        <div>
+            Profile Page
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum,
+                et!
+            </p>
+        </div>
+    );
 }
