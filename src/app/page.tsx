961f7538d0e0080158938878dda9818445a2a1e9
SanjarKeySkills <forest2004@mail.ru>
2025-06-12 12:15:11 +0600

    12/06/25| Something for everyone

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,6 +18,7 @@ export default function Home() {
                             Lorem ipsum dolor sit amet consectetur adipisicing
                             elit. Cupiditate itaque repudiandae rerum?
                         </p>
+                        <h2>Something for everyone</h2>
                         <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
                             src/app/page.tsx
                         </code>
